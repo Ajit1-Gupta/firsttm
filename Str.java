fd60ac49bb69b144b6770246a0baa686d2659a4a
Ajit Gupta <ajitg870211@gmail.com>
2023-12-01 14:15:49 +0530

    local to system

---
+++ Str.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Str {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the Strint :");
+        String as = sc.nextLine();
+        System.out.println("You have entered :" +as);
+    }
+    
+}
